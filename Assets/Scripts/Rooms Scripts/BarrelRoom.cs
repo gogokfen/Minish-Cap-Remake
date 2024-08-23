de5da71a185420e3604c9e8b4560018e5555922f
Oron Gefen <gogokfen@yahoo.com>
2024-08-24 01:35:32 +0300

    Final Push #4
    >remade the Boss Room, enlarged, added goo
    >added colliders above fences to prevent skipping rooms
    >Barrel room is 1000% cooler (added vines, fire, new dialogue)

--- Assets/Scripts/Rooms Scripts/BarrelRoom.cs
+++ Assets/Scripts/Rooms Scripts/BarrelRoom.cs
@@ -3,51 +3,66 @@ using UnityEngine.Events;
 public class BarrelRoom : MonoBehaviour
 {
     private int buttonsClicked = 0;
-    private int rightClicked = 0;
+    private int leftClicked = 0;
+    private bool sideComplete;
+    private float timer;
     [SerializeField] UnityEvent barrelDropEvent;
     [SerializeField] PressureButton pressureButton1;
     [SerializeField] PressureButton pressureButton2;
-    [SerializeField] GameObject brazierFlameRight;
+    [SerializeField] Animator leftBrazier;
+    [SerializeField] Animator rightBrazier;
+
+    private void Update()
+    {
+        if (sideComplete)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer >= 5)
+        {
+            buttonsClicked++;
+            timer = 0f;
+            sideComplete = false;
+            if (buttonsClicked == 1)
+            {
+                SFXController.PlaySFX("Secret", 0.5f);
+                Dialogue.StartDialogue(3);
+            }
+            DropTheBarrel();
+        }
+    }
+
     public void FinishRight()
     {
-        buttonsClicked++;
-        DropTheBarrel();
-        // if (buttonsClicked == 3)
-        // {
-        //     //barrel.transform.position += new Vector3 (0, -3, 0);
-        //     barrelDropEvent.Invoke();
-        //     // barrelTriggerOne.SetActive(true);
-        //     // barrelTriggerTwo.SetActive(true);
-        //     Destroy(pressureButton1);
-        //     Destroy(pressureButton2);
-        //     SFXController.PlaySFX("Secret", 0.5f);
-        //     Destroy(this);
-        // }
+        Movement.Scene(5);
+        rightBrazier.Play("RightBrazier");
+        sideComplete = true;
     }
     public void FinishLeft()
     {
-        rightClicked++;
-        if (rightClicked == 2)
+        leftClicked++;
+        if (leftClicked == 2)
         {
-            buttonsClicked++;
-            brazierFlameRight.SetActive(true);
             Destroy(pressureButton1);
             Destroy(pressureButton2);
+            sideComplete = true;
+            Movement.Scene(5);
+            leftBrazier.Play("LeftBrazier");
         }
-        DropTheBarrel();
     }
     public void DropTheBarrel()
     {
         if (buttonsClicked == 2)
         {
             barrelDropEvent.Invoke();
-            Dialogue.StartDialogue(3);
+            Dialogue.StartDialogue(4);
             SFXController.PlaySFX("Secret", 0.5f);
             Destroy(this);
         }
     }
     public void ButtonUp()
     {
-        rightClicked--;
+        leftClicked--;
     }
 }
